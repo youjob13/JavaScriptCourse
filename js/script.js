03f90ee81cedda811ab094bcec72c4f9b41d730b
Danil Rodionow <danil.rodionow13@gmail.com>
2020-08-07 10:01:07 +0300

    Добавил файлы в коммит

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const money = 10000;
+const income = 'Стипендия';
+const addExpenses = 'На покушать, На отдохнуть, На коммуналку'
+const deposit = true;
+const mission = 100000;
+const period = 4;
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+
+console.log(addExpenses.length);
+
+console.log('Период равен ' + period + ' месяцам');
+console.log('Цель заработать ' + mission + ' рублей');
+
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split());
+
+const budgetDay = money / 30;
+console.log(budgetDay);
