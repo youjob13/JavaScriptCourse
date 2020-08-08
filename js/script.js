c8fff0b13fdec3e2cb877d2b981f49b4a0aebfae
Danil Rodionow <danil.rodionow13@gmail.com>
2020-08-08 09:14:45 +0300

    fix(script.js): changed the output of the array

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const money = 10000;
 const income = 'Стипендия';
-const addExpenses = 'На покушать, На отдохнуть, На коммуналку'
+let addExpenses = 'На покушать, На отдохнуть, На коммуналку';
 const deposit = true;
 const mission = 100000;
 const period = 4;
@@ -14,8 +14,8 @@ console.log(addExpenses.length);
 console.log('Период равен ' + period + ' месяцам');
 console.log('Цель заработать ' + mission + ' рублей');
 
-console.log(addExpenses.toLowerCase());
-console.log(addExpenses.split());
+console.log(addExpenses = addExpenses.toLowerCase());
+console.log(addExpenses.split([,[]]));
 
 const budgetDay = money / 30;
 console.log(budgetDay);
