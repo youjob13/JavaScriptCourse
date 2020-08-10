9a5985f9de0b6bafe4820b35eb107e4e45d8fca8
Danil Rodionow <danil.rodionow13@gmail.com>
2020-08-11 00:12:54 +0300

    fix(script): fixed bugs

--- js/script.js
+++ js/script.js
@@ -33,9 +33,9 @@ console.log('Ваш бюджет на день: ' + budgetDay);
 
 if (budgetDay >= 1200) {
     console.log('У вас высокий уровень дохода');
-} else if (budgetDay >= 600 && budgetDay <= 1200) {
+} else if (budgetDay >= 600 && budgetDay < 1200) {
     console.log('У вас средний уровень дохода');
-} else if (budgetDay >= 0 && budgetDay <= 600) {
+} else if (budgetDay >= 0 && budgetDay < 600) {
     console.log('К сожалению у вас уровень дохода ниже среднего');
 } else {
     console.log('Что то пошло не так');
