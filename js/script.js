fb5c5fb05b4ef1c54013c8d33fed2d63b2ca50e2
Danil Rodionow <danil.rodionow13@gmail.com>
2020-08-11 11:18:28 +0300

    feat(script): completed the homework

---
+++ js/script.js
@@ -0,0 +1,43 @@
+'use strict'
+
+let period,
+    budgetDay,
+    accumulatedMonth;
+const income = 'Стипендия',
+      addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую'),
+      deposit = confirm('Есть ли у вас депозит в банке?'),
+      money = +prompt('Ваш ежемесячный доход?'),
+      expenses1 = prompt('Введите обязательную статью расходов?'),
+      expenses2 = prompt('Введите обязательную статью расходов?'),
+      amount1 = +prompt('Во сколько это обойдется?'),
+      amount2 = +prompt('Во сколько это обойдется?');
+const mission = 100000;
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+
+function getExpensesMonth() {
+    return amount1 + amount2;
+ }
+console.log('Расходы за месяц: ' + getExpensesMonth() + ' руб.');
+
+console.log(addExpenses.toLowerCase().split([,[]]));
+
+function getAccumulatedMonth() {
+    return accumulatedMonth = money - amount1 - amount2;
+}
+getAccumulatedMonth();
+
+function getTargetMonth() {
+    return  period = Math.ceil(mission / accumulatedMonth);
+}
+getTargetMonth();
+console.log('Цель будет достигнута за: ' + period + ' месяцев');
+
+console.log('Ваш бюджет на день: ' + (budgetDay = Math.floor(accumulatedMonth / 30)) + ' руб.');
+
+function getStatusIncome() {
+    return income;
+}
+console.log(getStatusIncome());
