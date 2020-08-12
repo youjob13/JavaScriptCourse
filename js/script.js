880e2b2a2e893f7e28556684911fe19609207337
Danil Rodionow <danil.rodionow13@gmail.com>
2020-08-12 14:05:27 +0300

    fix(script): fixed bug

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 function getString(string) {
     if (typeof string !== 'string') {
         console.log('Это не строка');
+    } else if (string.slice(0, 1) == 1 || string.slice(0, 1) == 2 || string.slice(0, 1) == 3 || string.slice(0, 1) == 4 || string.slice(0, 1) == 5
+    || string.slice(0, 1) == 6 || string.slice(0, 1) == 7 || string.slice(0, 1) == 8 || string.slice(0, 1) == 9 || string.slice(0, 1) == '0') {
+        console.log('Это не строка');
     } else {
         console.log(string.trim());
         if (string.length > 30) {
@@ -8,4 +11,4 @@ function getString(string) {
         }
     }
 }
-getString('          Privet Vasya Vakulenko ti krutoy reper');
+getString('   Лучшее решение в голову не пришло)) ');
