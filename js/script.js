2f5956509f4582f08dad98aa92b2530256007e17
Danil Rodionow <danil.rodionow13@gmail.com>
2020-08-22 18:37:41 +0300

    feat(all):created project

---
+++ js/script.js
@@ -0,0 +1,68 @@
+'use strict';
+
+const todoControl = document.querySelector('.todo-control'),
+    headerInput = document.querySelector('.header-input'),
+    todoList = document.querySelector('.todo-list'),
+    todoCompleted = document.querySelector('.todo-completed'),
+    p = document.querySelector('#hel');
+const todoData = [];
+
+const render = function () {
+    todoList.textContent = '';
+    todoCompleted.textContent = '';
+   
+    todoData.forEach(function (item,i) {
+        
+    localStorage.setItem(i,item.value);   
+        const li = document.createElement('li');
+        li.classList.add('todo-item');
+        li.innerHTML = '<span class="text-todo">' + localStorage.getItem(i) + '</span>' +
+            '<div class="todo-buttons">' +
+            '<button class="todo-remove"></button>' +
+            '<button class="todo-complete"></button>' +
+            '</div>';
+
+        if (item.completed) {
+            todoCompleted.append(li);
+        } else {
+            todoList.append(li);
+        }
+
+
+        const btnTodoCompleted = li.querySelector('.todo-complete');
+
+        btnTodoCompleted.addEventListener('click', function () {
+            item.completed = !item.completed;
+            render();
+        });
+
+
+        const btnTodoDelete = li.querySelector('.todo-remove');
+
+        btnTodoDelete.addEventListener('click', function () {
+            todoData.pop(item);
+            localStorage.removeItem(i);
+            render();
+        });
+        console.log(todoData);
+
+    });
+    
+};
+
+todoControl.addEventListener('submit', function (event) {
+    event.preventDefault();
+    if (headerInput.value.trim() !== '') {
+        const newTodo = {
+            value: headerInput.value,
+            completed: false,
+        };
+        todoData.push(newTodo);
+        headerInput.value = null;
+        render();
+    } else {
+        alert('write');
+        headerInput.value = null;
+    }
+});
+render();
