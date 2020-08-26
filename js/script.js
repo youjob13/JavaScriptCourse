5552da44b67680231b8a35e90adda1692e31794d
Danil Rodionow <danil.rodionow13@gmail.com>
2020-08-27 01:03:21 +0300

    fix(script):fixed the bugs

--- js/script.js
+++ js/script.js
@@ -1,31 +1,25 @@
-function DomElement(selector, height, width, bg, fontSize) {
+class DomElement {
+  constructor(selector, height, width, bg, fontSize) {
     this.selector = selector;
     this.height = height;
     this.width = width;
     this.bg = bg;
     this.fontSize = fontSize;
     this.created = function (selector) {
-
-        if (selector.trim().slice(0,1) == '.') {
-            let elem = document.createElement('div');
-            elem.classList.add('elem');
-            elem.textContent = 'А теперь можно и покушать';
-            elem.style.cssText = `height: ${height}; width: ${width}; background: ${bg}; font-size: ${fontSize};`;
-            
-            document.body.append(elem);
-            
-        } else if (selector.trim().slice(0, 1) == '#') {
-            let elem = document.createElement('id');
-            elem.id = 'elem';
-            elem.classList.add('elem');
-            elem.style.cssText = `height: ${height}; width: ${width}; background: ${bg}; font-size: ${fontSize};`;
-            elem.textContent = 'Сегодня голодаем';
-            document.body.append(elem);
-        }
-
+      const block =
+        selector.trim().slice(0, 1) == "."
+          ? document.createElement("div")
+          : selector.trim().slice(0, 1) == "#"
+          ? document.createElement("id")
+          : alert("не то");
+      block.classList.add("block");
+      block.textContent = "Покушал, набрался сил";
+      block.style.cssText = `height: ${height}; width: ${width}; background: ${bg}; font-size: ${fontSize};`;
+      document.body.append(block);
     };
+  }
 }
 
-let bg1 = new DomElement(".blocked", '150px', '100%', 'brown','45px');
-
-bg1.created('.blocked');
+const bg1 = new DomElement("#blocked", "150px", "100%", "brown", "45px");
+console.log(bg1);
+bg1.created("#blocked");
