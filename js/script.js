4226052b5da9c163c6cafddd6d091899dae43bac
Danil Rodionow <danil.rodionow13@gmail.com>
2020-08-27 22:22:57 +0300

    fix(script): created method "created" using prototype

--- js/script.js
+++ js/script.js
@@ -1,46 +1,49 @@
 document.addEventListener("DOMContentLoaded", function () {
-    alert("DOM готов!");
+  alert("DOM готов!");
 
-    class DomElement {
-        constructor(selector, height, width, bg, position) {
-            this.selector = selector;
-            this.height = height;
-            this.width = width;
-            this.bg = bg;
-            this.position = position;
-            this.created = function () {
-
-                let elem = document.createElement('id');
-                elem.id = 'elem';
-                elem.classList.add('elem');
-                elem.style.cssText = `height: ${height}; width: ${width}; background: ${bg}; position: ${position};`;
-                elem.innerHTML = '<br>    __人____人 <br> ≧(◕‿‿◕)≦ ';
-                document.body.append(elem);
-
-            };
-        }
+  class DomElement {
+    constructor(
+      height = "100px",
+      width = "100px",
+      bg = "red",
+      position = "relative"
+    ) {
+      this.height = height;
+      this.width = width;
+      this.bg = bg;
+      this.position = position;
     }
+  }
 
-    let bg1 = new DomElement(".blocked", '100px', '100px', 'grey', 'absolute');
-    bg1.created('.blocked');
-    document.addEventListener('keydown', function (e) {
-        e.preventDefault();
-        switch (event.key) {
-            case "ArrowLeft":
-                elem.style.transform += 'translateX(-10px)';
-                break;
-            case "ArrowRight":
-                elem.style.transform += 'translateX(10px)';
-                break;
-            case "ArrowUp":
-                elem.style.transform += 'translateY(-10px)';
-                break;
-            case "ArrowDown":
-                elem.style.transform += 'translateY(10px)';
-                break;
-        }
+  const bg1 = new DomElement("100px", "100px", "grey", "absolute");
 
-    });
+  DomElement.prototype.created = function () {
+    const elem = document.createElement("div");
+    elem.id = "elem";
+    elem.style.cssText = `height: ${this.height}; width: ${this.width}; background: ${this.bg}; position: ${this.position};`;
+    elem.innerHTML = ` 
+    __人____人 
+    ≧(◕‿‿◕)≦ `;
+    document.body.append(elem);
+  };
 
+  bg1.created();
 
+  document.addEventListener("keydown", function (e) {
+    e.preventDefault();
+    switch (event.key) {
+      case "ArrowLeft":
+        elem.style.transform += "translateX(-10px)";
+        break;
+      case "ArrowRight":
+        elem.style.transform += "translateX(10px)";
+        break;
+      case "ArrowUp":
+        elem.style.transform += "translateY(-10px)";
+        break;
+      case "ArrowDown":
+        elem.style.transform += "translateY(10px)";
+        break;
+    }
+  });
 });
