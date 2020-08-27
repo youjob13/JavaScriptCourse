3954f13771dbe0664f75f41c9ed0c671e41deabd
Danil Rodionow <danil.rodionow13@gmail.com>
2020-08-27 20:51:23 +0300

    fix(script): fixed the bugs

--- js/script.js
+++ js/script.js
@@ -1,25 +1,25 @@
 class DomElement {
-  constructor(selector, height, width, bg, fontSize) {
+  constructor(selector = "#div", height = 320, width = 320, bg = "red", fontSize = 32) {
     this.selector = selector;
     this.height = height;
     this.width = width;
     this.bg = bg;
     this.fontSize = fontSize;
-    this.created = function (selector) {
-      const block =
-        selector.trim().slice(0, 1) == "."
-          ? document.createElement("div")
-          : selector.trim().slice(0, 1) == "#"
-          ? document.createElement("id")
-          : alert("не то");
-      block.classList.add("block");
-      block.textContent = "Покушал, набрался сил";
-      block.style.cssText = `height: ${height}; width: ${width}; background: ${bg}; font-size: ${fontSize};`;
-      document.body.append(block);
-    };
+  }
+  created() {
+    const block =
+      this.selector.trim().slice(0, 1) == "."
+        ? document.createElement("div")
+        : this.selector.trim().slice(0, 1) == "#"
+        ? document.createElement("id")
+        : alert("не то");
+    block.classList.add("block");
+    block.textContent = "Покушал, набрался сил";
+    block.style.cssText = `height: ${this.height}; width: ${this.width}; background: ${this.bg}; font-size: ${this.fontSize};`;
+    document.body.append(block);
   }
 }
 
-const bg1 = new DomElement("#blocked", "150px", "100%", "brown", "45px");
+const bg1 = new DomElement(".blocked", "150px", "100%", "brown", "45px");
 console.log(bg1);
-bg1.created("#blocked");
+bg1.created();
