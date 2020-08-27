d005d9418d975939ee8b73436251bf4128b1fe03
Danil Rodionow <danil.rodionow13@gmail.com>
2020-08-27 11:10:39 +0300

    feat(all):completed the task

---
+++ js/script.js
@@ -0,0 +1,18 @@
+class First {
+
+    hello() {
+        console.log('Привет я метод родителя!');
+    }
+
+}
+class Second extends First {
+
+    hello() {
+        super.hello();
+        console.log('А я наследуемый метод!');
+    }
+    
+}
+
+const obj = new Second();
+obj.hello();
