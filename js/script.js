6f55fe1a5e2bb25735742516d7ac9024bb2b85fb
Danil Rodionow <danil.rodionow13@gmail.com>
2020-08-27 22:14:29 +0300

    fix(script):created a method "created" using a prototype

--- js/script.js
+++ js/script.js
@@ -1,25 +1,32 @@
 class DomElement {
-  constructor(selector = "#div", height = 320, width = 320, bg = "red", fontSize = 32) {
+  constructor(
+    selector = "#div",
+    height = "320px",
+    width = "320px",
+    bg = "red",
+    fontSize = "32px"
+  ) {
     this.selector = selector;
     this.height = height;
     this.width = width;
     this.bg = bg;
     this.fontSize = fontSize;
   }
-  created() {
-    const block =
-      this.selector.trim().slice(0, 1) == "."
-        ? document.createElement("div")
-        : this.selector.trim().slice(0, 1) == "#"
-        ? document.createElement("id")
-        : alert("не то");
-    block.classList.add("block");
-    block.textContent = "Покушал, набрался сил";
-    block.style.cssText = `height: ${this.height}; width: ${this.width}; background: ${this.bg}; font-size: ${this.fontSize};`;
-    document.body.append(block);
-  }
 }
 
+DomElement.prototype.created = function () {
+  const block =
+    this.selector.trim().slice(0, 1) == "."
+      ? document.createElement("div")
+      : this.selector.trim().slice(0, 1) == "#"
+        ? document.createElement("p")
+        : alert("не то");
+  block.classList.add("block");
+  block.textContent = "Покушал, набрался сил";
+  block.style.cssText = `height: ${this.height}; width: ${this.width}; background: ${this.bg}; font-size: ${this.fontSize};`;
+  document.body.append(block);
+};
+
 const bg1 = new DomElement(".blocked", "150px", "100%", "brown", "45px");
-console.log(bg1);
+
 bg1.created();
