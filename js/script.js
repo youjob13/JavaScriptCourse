c022622d7240c788f122154c9ce3e4cadd4a9403
Danil Rodionow <danil.rodionow13@gmail.com>
2020-08-30 23:06:08 +0300

    feat(script): optimized code

--- js/script.js
+++ js/script.js
@@ -1,18 +1,13 @@
-'use strict';
-const btn = document.querySelector('button');
-const colorCode = document.querySelector('h2');
-let result = '';
+"use strict";
+const btn = document.querySelector("button");
+const colorCode = document.querySelector("h2");
+let result = "";
 const changeBg = () => {
-    let r,
-        b,
-        g,
-        rgb;
-    r = Math.floor(Math.random() * (256));
-    g = Math.floor(Math.random() * (256));
-    b = Math.floor(Math.random() * (256));
-    rgb = '#' + r.toString(16) + g.toString(16) + b.toString(16);
-    console.log(rgb);
-    colorCode.textContent = rgb;
-    document.body.style.backgroundColor = rgb;
+  let r = Math.floor(Math.random() * 256),
+    b = Math.floor(Math.random() * 256),
+    g = Math.floor(Math.random() * 256),
+    rgb = "#" + r.toString(16) + g.toString(16) + b.toString(16);
+  colorCode.textContent = rgb;
+  document.body.style.backgroundColor = rgb;
 };
-btn.addEventListener('click', () => changeBg());
+btn.addEventListener("click", () => changeBg());
