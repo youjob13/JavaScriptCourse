6af8ca2ac0bf2ce2185fca656e2ee260e1247634
Danil Rodionow <danil.rodionow13@gmail.com>
2020-08-31 11:20:11 +0300

    feat(script): localStorage

--- js/script.js
+++ js/script.js
@@ -1,9 +1,6 @@
-const signUpBtn = document.querySelector('.sign_up');
-const signInBtn = document.querySelector('.sign_in');
-const usersList = document.querySelector('#list');
-
-const regDate = new Date();
-const database = [];
+const signUpBtn = document.querySelector(".sign_up");
+const signInBtn = document.querySelector(".sign_in");
+const usersList = document.querySelector("#list");
 
 const optionsMonth = {
     month: "long",
@@ -11,7 +8,7 @@ const optionsMonth = {
 
 const checkTime = (time) => {
     if (time < 10) {
-        return '0' + time ;
+        return "0" + time;
     } else {
         return time;
     }
@@ -19,19 +16,36 @@ const checkTime = (time) => {
 
 const dateToday = () => {
     let monthLength = regDate.toLocaleString("ru", optionsMonth).length;
-    let today = regDate.getDate() + " " + regDate.toLocaleString("ru", optionsMonth) + " " + regDate.getFullYear() + ' г. ' +
-        checkTime(regDate.getHours()) + ':' + checkTime(regDate.getMinutes()) + ':' + checkTime(regDate.getSeconds());
+    let today =
+        regDate.getDate() +
+        " " +
+        regDate.toLocaleString("ru", optionsMonth) +
+        " " +
+        regDate.getFullYear() +
+        " г. " +
+        checkTime(regDate.getHours()) +
+        ":" +
+        checkTime(regDate.getMinutes()) +
+        ":" +
+        checkTime(regDate.getSeconds());
     return today;
 };
 
+
+const regDate = new Date();
+const database = localStorage['database'] ? JSON.parse(localStorage['database']) : [];
+
+
 const registration = () => {
-    const name = prompt('Введите через пробел Имя и Фамилию пользователя', 'Данил Родионов');
-    const firstName = name.split(' ');
-    const login = prompt('Придумайте логин', 'YouJOB');
-    const password = prompt('Придумайте пароль', '12345');
-    const user = document.createElement('li');
-    user.textContent = 'Имя: ' + firstName[0] + ', Фамилия: ' + firstName[1] + ', Дата регистрации: ' + dateToday();
-    usersList.append(user);
+    const name = prompt(
+        "Введите через пробел Имя и Фамилию пользователя",
+        "Адиль Жалелов"
+    );
+
+    const firstName = name.split(" ");
+    const login = prompt("Придумайте логин", "YouJOB");
+    const password = prompt("Придумайте пароль", "12345");
+
     const usersAcc = {
         firstName: firstName[0],
         lastName: firstName[1],
@@ -40,8 +54,12 @@ const registration = () => {
         date: dateToday(),
     };
     database.push(usersAcc);
-    console.log(database);
 
-    console.log(usersAcc);
+    const user = document.createElement("li");
+    user.textContent = usersAcc.firstName + ' ' + usersAcc.lastName + ' ' + usersAcc.date;
+    usersList.append(user);
+    localStorage['database'] = JSON.stringify(database);
+
 };
-signUpBtn.addEventListener('click', registration);
+
+signUpBtn.addEventListener("click", registration);
