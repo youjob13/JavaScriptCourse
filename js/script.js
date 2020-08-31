fcab3038e6febbc9decb9cff5aa37e7cec3484fa
Danil Rodionow <danil.rodionow13@gmail.com>
2020-08-31 14:35:46 +0300

    feat(script): completed the task

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const signUpBtn = document.querySelector(".sign_up");
 const signInBtn = document.querySelector(".sign_in");
 const usersList = document.querySelector("#list");
+const welcome = document.querySelector('.hello');
 
 const optionsMonth = {
     month: "long",
@@ -29,39 +30,68 @@ const registration = () => {
 
     const name = prompt("Введите через пробел Имя и Фамилию пользователя", "Адиль Жалелов");
     const names = name.split(" ");
+    console.log(names);
+    if (names === '' || names.length > 2 || names.length < 2) {
+        alert('Введите корректные данные');
+    } else {
+        const usersAcc = {
+            firstName: names[0],
+            lastName: names[1],
+            login: prompt("Придумайте логин", "YouJOB"),
+            password: prompt("Придумайте пароль", "12345"),
+            date: regDate.getDate() +
+                " " +
+                checkMonth(regDate.toLocaleString("ru", optionsMonth)) +
+                " " +
+                regDate.getFullYear() +
+                " г. " +
+                checkTime(regDate.getHours()) +
+                ":" +
+                checkTime(regDate.getMinutes()) +
+                ":" +
+                checkTime(regDate.getSeconds()),
+        };
 
-    const usersAcc = {
-        firstName: names[0],
-        lastName: names[1],
-        login: prompt("Придумайте логин", "YouJOB"),
-        password: prompt("Придумайте пароль", "12345"),
-        date: regDate.getDate() +
-            " " +
-            checkMonth(regDate.toLocaleString("ru", optionsMonth)) +
-            " " +
-            regDate.getFullYear() +
-            " г. " +
-            checkTime(regDate.getHours()) +
-            ":" +
-            checkTime(regDate.getMinutes()) +
-            ":" +
-            checkTime(regDate.getSeconds()),
-    };
+        database.push(usersAcc);
+        render();
+    }
 
-    database.push(usersAcc);
-    render();
-    localStorage['database'] = JSON.stringify(database);
 
+};
+
+const authorization = () => {
+    const login = prompt('Введите логин:');
+    const password = prompt('Введите пароль:');
+    database.forEach((item, i, arr) => {
+        if (login === arr[i].login && password === arr[i].password) {
+            welcome.textContent = `Привет ${arr[i].firstName}`;
+        } else if (login === arr[i].login || password === arr[i].password) {
+            alert('Ошибка');
+        }
+    });
 };
 
 const render = () => {
     usersList.textContent = ''
     database.forEach((item, i, arr) => {
         const user = document.createElement("li");
+        const deleteBtn = document.createElement("button");
+        deleteBtn.classList.add('btn-delete')
         user.textContent = `Имя: ${arr[i].firstName}, Фамилия: ${arr[i].lastName}, Дата регистрации: ${arr[i].date}`;
         usersList.append(user);
+        user.append(deleteBtn);
+
+        deleteBtn.addEventListener('click', () => {
+            database.pop(item);
+            localStorage.removeItem('database');
+            welcome.textContent = 'Привет аноним';
+            render();
+        });
+        localStorage['database'] = JSON.stringify(database);
+
     });
 };
 
 render();
 signUpBtn.addEventListener("click", registration);
+signInBtn.addEventListener("click", authorization);
