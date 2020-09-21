952e0b302fa47617c1613f890fd99f502c6b37db
Danil Rodionow <danil.rodionow13@gmail.com>
2020-09-21 14:36:34 +0300

    fix(all):final changes

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 "use strict";
 const header = document.querySelector(".link"),
     outputMovies = document.querySelector(".output-movies"),
+    linkOutput = document.querySelector('.link-output'),
     selectMovies = document.getElementById("select-movies");
 
 fetch("dbHeroes.json", {
@@ -40,9 +41,9 @@ const choosedMovies = (movies) => {
     arr = arr.filter((item, i) => arr.indexOf(item) === i);
     arr.forEach((item) => {
         selectMovies.options[selectMovies.selectIndex];
-        let newOption = new Option(item, item);
-        selectMovies.append(newOption);
-        newOption.selected = true;
+        const newOption2 = new Option(item, item);
+        selectMovies.append(newOption2);
+        newOption2.selected = true;
     });
     return arr;
 };
@@ -61,7 +62,7 @@ const addCard = ({
 }) => {
     const card = document.createElement("li");
     card.classList.add("heroes");
-    card.innerHTML = `<div class="heroes-info"><strong>Имя:</strong> ${name}<br>
+    card.innerHTML = `<a name="${name}"><div class="heroes-info"><strong>Имя:</strong> ${name}<br>
         <strong>Настоящее имя:</strong> ${realName} <br>
         <strong>Раса:</strong> ${species} <br>
         <strong>Гражданство:</strong> ${citizenship}<br>
@@ -70,13 +71,17 @@ const addCard = ({
         <strong>Статус:</strong> ${status} <br> 
         <strong>Актер:</strong> ${actors} <br>
         <strong>Фильмы:</strong> ${movies}</div>
-        <img class = "photo" src = "${photo}">`;
+        <img class = "photo" src = "${photo}"></a>`;
     outputMovies.append(card);
+    const nameHeroes = document.createElement("li");
+    nameHeroes.innerHTML = `<a class="link-name" href="#${name}"> ${name} </a>`;
+    linkOutput.append(nameHeroes);
     header.textContent = selectMovies.value;
 };
 
 const filterMovies = (heroesArray) => {
     outputMovies.textContent = "";
+    linkOutput.textContent = "";
     heroesArray.forEach((item) => {
         if (item.movies) {
             item.movies.filter((elem) => {
