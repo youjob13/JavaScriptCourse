ba68e7192f58969f92996cff97849309767a9985
Danil Rodionow <danil.rodionow13@gmail.com>
2020-09-23 20:41:51 +0300

    feat(script):added function - toggle popupRepair

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@
 
 import showPhone from './modules/showPhone';
 import menu from './modules/menu';
+import popupRepair from './modules/popupRepair';
+
 
 showPhone();
 menu();
+popupRepair();
---
+++ src/modules/popupRepair.js
@@ -0,0 +1,22 @@
+const togglePopUpRepair = () => {
+  const popupRepair = document.querySelector(".popup-repair-types"),
+    popupRepairClose = popupRepair.querySelectorAll(".close");
+
+  document.addEventListener("click", (e) => {
+    if (
+      e.target.matches(".no-overflow") ||
+      e.target.matches(".desktop-hide a")
+    ) {
+      document.querySelector(".popup-dialog-menu").style.transform = null;
+      document.querySelector(".popup-dialog-menu").style.opacity = "0";
+      popupRepair.style.visibility = "visible";
+    }
+    popupRepairClose.forEach((item) => {
+      if (e.target === item) {
+        popupRepair.style.visibility = "hidden";
+      }
+    });
+  });
+};
+
+export default togglePopUpRepair;
