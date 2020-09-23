a902ba3d0f811c8b1811ab9f80fa2d27decdc5d8
Danil Rodionow <danil.rodionow13@gmail.com>
2020-09-23 18:04:16 +0300

    feat(script):show phone list

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
 'use strict';
+
+import menu from './modules/menu';
+
+menu();
---
+++ src/modules/menu.js
@@ -0,0 +1,28 @@
+const addMenu = () => {
+    const arrow = document.querySelector('.header-contacts__arrow'),
+        numberPhoneAccord = document.querySelector('.header-contacts__phone-number-accord'),
+        numberPhone = numberPhoneAccord.querySelector('.header-contacts__phone-number');
+
+    const stylePhone = document.createElement('style');
+    document.querySelector('head').append(stylePhone);
+
+    stylePhone.textContent = `
+    .position {
+        position: relative!important;
+    }
+    .opacity {
+        opacity: 1!important;
+    }
+    .arrow-rotate {
+        transform: rotateZ(180deg) !important;
+    }
+    `;
+    
+    arrow.addEventListener('click', () => {
+        numberPhoneAccord.classList.toggle('position');
+        numberPhone.classList.toggle('opacity');
+        arrow.classList.toggle('arrow-rotate');
+    });
+};
+
+export default addMenu;
