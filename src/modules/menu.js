f375c2f8478484549dcf5109e3eee24f111b4e65
Danil Rodionow <danil.rodionow13@gmail.com>
2020-09-23 19:44:16 +0300

    feat(script):added adaptive menu

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,16 +1,16 @@
 const toggleMenu = () => {
+  const menu = document.querySelector(".popup-dialog-menu");
 
-    const menu = document.querySelector('.popup-dialog-menu');
-    
-    document.addEventListener('click', (e) => {
-        if (e.target.matches('.menu__icon')) {
-            menu.style.transform = 'translate3d(0, 0, 0)';
-        }
-        if (e.target.matches('.close-menu')) {
-            menu.style.transform = 'translate3d(645px, 0, 0)';
-        }
-    });
-
+  document.addEventListener("click", (e) => {
+    if (e.target.matches(".menu__icon")) {
+      menu.style.transform = "translate3d(0, 0, 0)";
+      menu.style.opacity = "1";
+    }
+    if (e.target.matches(".close-menu")) {
+      menu.style.transform = null;
+      menu.style.opacity = "0";
+    }
+  });
 };
 
 export default toggleMenu;
