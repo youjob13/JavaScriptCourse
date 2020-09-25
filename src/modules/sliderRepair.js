c31868311d9d063dfcd59b36d22a7ccab4dd7202
Danil Rodionow <danil.rodionow13@gmail.com>
2020-09-25 21:28:23 +0300

    fix(script):fixed the bug in sliderRepair

--- src/modules/sliderRepair.js
+++ src/modules/sliderRepair.js
@@ -1,15 +1,20 @@
 const sliderRepair = () => {
   const slider = document.querySelector(".repair-types-slider"),
+    typesRepairSlider = slider.children,
+    btnRepair = document
+      .querySelector(".repair-types-nav")
+      .querySelectorAll(".repair-types-nav__item"),
+    sliderCounterCurrent = document
+      .querySelector(".repair-types-slider-wrap")
+      .querySelector(".slider-counter-content__current"),
     style = document.querySelector("style"),
-    btnRepair = document.querySelectorAll(".repair-types-nav__item"),
-    counterSlider = document
-    .querySelector(".repair-types-slider-wrap")
-    .querySelector(".slider-counter-content__current"),
-    slideArr = slider.querySelector(".types-repair1").querySelectorAll("img");
-  let currentSlide = 0;
+    sliderCounterTotal = document
+      .querySelector(".repair-types-slider-wrap")
+      .querySelector(".slider-counter-content__total");
 
-  slideArr[0].classList.add("slide-portfolio-active");
-  style.textContent = style.textContent + `
+  style.textContent =
+    style.textContent +
+    `
         .slide-portfolio-active {
             display: block !important; 
         }
@@ -21,57 +26,128 @@ const sliderRepair = () => {
           overflow:hidden;
         }
     }`;
+  let currentSlide = 0,
+    currentSlider = typesRepairSlider[0],
+    currentSliderLength = currentSlider.children.length,
+    arraySlides = currentSlider.querySelectorAll("img");
+  sliderCounterTotal.textContent = currentSliderLength;
+  arraySlides[0].classList.add("slide-portfolio-active");
 
   const nextSlide = (elem, index, link) => {
+    elem[index].classList.add(link);
+  };
+  const prevSlide = (elem, index, link) => {
+    elem[index].classList.remove(link);
+  };
+
+  const activeBtnRepair = (index) => {
     if (document.body.clientWidth < 1024) {
-      style.textContent = style.textContent + `
+      style.textContent =
+        style.textContent +
+        `
       @media(max-width: 1080px) {
       .nav-list-repair {
-        transform: translateX(-${btnRepair[index].offsetWidth * index}px);
+        transform: translateX(-${btnRepair[index].offsetLeft}px);
       };
     }`;
     }
     btnRepair[index].classList.add("active");
-    elem[index].classList.add(link);
+
+    currentSlider = typesRepairSlider[index];
+    arraySlides = currentSlider.querySelectorAll("img");
+    arraySlides[0].classList.add("slide-portfolio-active");
+    currentSliderLength = currentSlider.children.length;
+    currentSlide = 0;
   };
-  const prevSlide = (elem, index, link) => {
+  const passiveBtnRepair = (index) => {
     btnRepair[index].classList.remove("active");
-    elem[index].classList.remove(link);
   };
 
   document.addEventListener("click", (e) => {
-    prevSlide(slideArr, currentSlide, "slide-portfolio-active");
+    //табы + слайдер для табов
     if (
-      e.target.closest("#repair-types-arrow_right") ||
-      e.target.closest("#nav-arrow-repair-right_base")
-    ) {
-      currentSlide++;
-    } else if (
-      e.target.closest("#repair-types-arrow_left") ||
+      e.target.closest(".repair-types-nav") ||
+      e.target.closest("#nav-arrow-repair-right_base") ||
       e.target.closest("#nav-arrow-repair-left_base")
     ) {
-      currentSlide--;
-    }
-
-    btnRepair.forEach((item, i) => {
-      if (e.target === item) {
-        currentSlide = i;
+      if (e.target.closest(".repair-types-nav")) {
+        arraySlides[currentSlide].classList.remove("slide-portfolio-active");
+        btnRepair.forEach((item, i) => {
+          if (e.target === item) {
+            arraySlides[currentSlide].classList.remove(
+              "slide-portfolio-active"
+            );
+            activeBtnRepair(i);
+          } else {
+            passiveBtnRepair(i);
+          }
+        });
       }
-    });
 
-    if (currentSlide >= slideArr.length) {
-      currentSlide = 0;
+      if (e.target.closest("#nav-arrow-repair-right_base")) {
+        let count = 0;
+        arraySlides[currentSlide].classList.remove("slide-portfolio-active");
+        btnRepair.forEach((item, i) => {
+          if (item.classList.contains("active")) {
+            count = i;
+          }
+        });
+
+        if (count >= btnRepair.length - 1) {
+          count = 0;
+          activeBtnRepair(count);
+          passiveBtnRepair(btnRepair.length - 1);
+        } else {
+          activeBtnRepair(count + 1);
+          passiveBtnRepair(count);
+        }
+      }
+
+      if (e.target.closest("#nav-arrow-repair-left_base")) {
+        let count = 0;
+        arraySlides[currentSlide].classList.remove("slide-portfolio-active");
+        btnRepair.forEach((item, i) => {
+          if (item.classList.contains("active")) {
+            count = i;
+          }
+        });
+        if (count <= 0) {
+          count = btnRepair.length - 1;
+          passiveBtnRepair(0);
+          activeBtnRepair(count);
+        } else {
+          passiveBtnRepair(count);
+          activeBtnRepair(count - 1);
+        }
+      }
+      sliderCounterCurrent.textContent = currentSlide + 1;
+      sliderCounterTotal.textContent = currentSliderLength;
     }
-    if (currentSlide < 0) {
-      currentSlide = slideArr.length - 1;
-    }
+    //переключение слайдов
+    if (
+      e.target.closest("#repair-types-arrow_right") ||
+      e.target.closest("#repair-types-arrow_left")
+    ) {
+      currentSliderLength = currentSlider.children.length;
+      arraySlides = currentSlider.querySelectorAll("img");
 
-    nextSlide(slideArr, currentSlide, "slide-portfolio-active");
+      prevSlide(arraySlides, currentSlide, "slide-portfolio-active");
+      if (e.target.closest("#repair-types-arrow_right")) {
+        currentSlide++;
+      } else if (e.target.closest("#repair-types-arrow_left")) {
+        currentSlide--;
+      }
 
-    if (currentSlide >= 3) {
-      counterSlider.textContent = currentSlide - 2;
-    } else {
-      counterSlider.textContent = currentSlide + 1;
+      if (currentSlide >= arraySlides.length) {
+        currentSlide = 0;
+      }
+      if (currentSlide < 0) {
+        currentSlide = arraySlides.length - 1;
+      }
+      nextSlide(arraySlides, currentSlide, "slide-portfolio-active");
+
+      sliderCounterCurrent.textContent = currentSlide + 1;
+      sliderCounterTotal.textContent = currentSliderLength;
     }
   });
 };
