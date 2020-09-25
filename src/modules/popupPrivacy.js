493348e5e6d2a65a6e694a4ac5dd14d678092f3e
Danil Rodionow <danil.rodionow13@gmail.com>
2020-09-25 19:19:09 +0300

    fix(script):fixed the bug in popupPrivacy

--- src/modules/popupPrivacy.js
+++ src/modules/popupPrivacy.js
@@ -5,8 +5,10 @@ const togglePopUpPrivacy = () => {
     document.addEventListener('click', (e) => {
         if (e.target.matches('.link-privacy')) {
             popupPrivacy.style.visibility = 'visible';
+            document.body.style.overflow = 'hidden';
         } else if (e.target === closePopupPrivacy) {
             popupPrivacy.style.visibility = 'hidden';
+            document.body.style.overflow = 'auto';
         }
     });
 };
