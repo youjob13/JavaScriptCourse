394f9133d53db899f8a5a585ce99004d6a86bb78
Danil Rodionow <danil.rodionow13@gmail.com>
2020-09-28 21:22:52 +0300

    fix(script):fixed the bugs in sendForm

--- src/modules/sendForm.js
+++ src/modules/sendForm.js
@@ -11,9 +11,34 @@ const sendForm = () => {
     const forms = [];
     forms.push(form1, form2, form3, form4, form5, form6);
     forms.forEach(item => {
+        const phoneInput = item.querySelector('.feedback__input-input') || item.querySelector('.feedback-block__form-input_phone');
+        const nameInput = item.querySelector('.feedback-block__form-input_name');
         const checkboxInput = item.querySelector('.checkbox__input');
+        const btn = item.querySelector('button');
         item.addEventListener('submit', (e) => {
             e.preventDefault();
+            if (phoneInput.value.trim() === '' ||
+                (nameInput !== null &&
+                    nameInput.value.trim() === '')) {
+                const message = document.createElement('div');
+                message.classList.add('ac21');
+                message.style.cssText = `
+                font-size:17px;
+                font-family:sans-serif;
+                margin:auto;
+                padding:4px 8px;
+                border-radius:8px;
+                background-color:#e8e8e8;
+                color:#f68741;`
+                message.textContent = 'Заполните ВСЕ поля ввода перед отправкой';
+                btn.after(message);
+                btn.disabled = true;
+                setTimeout(() => {
+                    btn.disabled = false;
+                    message.parentNode.removeChild(message);
+                }, 2000);
+                return;
+            }
             if (!checkboxInput.checked) {
                 return;
             }
@@ -27,6 +52,8 @@ const sendForm = () => {
                 if (response.status !== 200) {
                     throw new Error('status network is not 200');
                 }
+                phoneInput.value = '';
+                nameInput.value = '';
                 popupThank();
             }).catch((error) => console.error(error));
         });
